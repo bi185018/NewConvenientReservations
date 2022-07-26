b886f7c01a72403c5ff01187f9f0ab1e28bdf6a6
Bura Iruku <bi185018@ncr.com>
2022-07-26 11:26:29 -0400

    Initial Commit

---
+++ NewConvenientReservations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewConvenientReservations
+//
+//  Created by Iruku, Bura on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewConvenientReservations/NewConvenientReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewConvenientReservationsApp.swift
+//  NewConvenientReservations
+//
+//  Created by Iruku, Bura on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct NewConvenientReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
